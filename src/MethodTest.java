30c4e9eaf8bcc8849d29eaa69610ae6df39f6861
W-C-G <55659912+W-C-G@users.noreply.github.com>
2021-04-05 18:25:35 +0900

    Practice of method making and data stream

--- src/MethodTest.java
+++ src/MethodTest.java
@@ -1,11 +1,12 @@
 import java.util.*;
+import com.tjoeun.oop.*;
 
 public class MethodTest {
+	static Random rd = new Random(); 
 
 	public static void main(String[] args) {
 		
 	// 문제 1
-	Random rd = new Random(); 
 	int a = rd.nextInt(10)+1; 
 	int b = rd.nextInt(10)+1;
 	int[] result = add(a, b);
@@ -22,13 +23,33 @@ public class MethodTest {
 
 	// 문제 4
 	int[] input2 = {2, 5, 6, 3, 2, 1};
-	System.out.println(Arrays.toString(evenlist(input2)));
+	//System.out.println(Arrays.toString(evenlist(input2)));
 	
 	// 문제 5
 	ArrayList<Integer> result3 = evenlist2(input2);
-	for(int i =0;i<result3.size();i++) {
-		System.out.println(result3.get(i));
-	}
+	/*
+	 * for(int i =0;i<result3.size();i++) { System.out.println(result3.get(i)); }
+	 */
+	
+	// 문제 6
+	String password = password();
+	
+	// 문제 7
+	System.out.println(sum2(5, 10));
+	
+	// 문제 8
+	createM(1, "smith").printList();
+	
+	// 문제 9
+	Member m1 = new Member(1, "smith", null, null);
+	Member m2 = new Member(2, "bob", null, null);
+	Member m3 = new Member(3, "kali", null, null);
+	ArrayList<Member> memlist = new ArrayList<>();
+	memlist.add(m1);
+	memlist.add(m2);
+	memlist.add(m3);
+	String[] names = getNames(memlist);
+	System.out.println(Arrays.toString(names));
 	}
 
 	/* 
@@ -96,5 +117,76 @@ public class MethodTest {
 			}
 		}
 		return result;
+	}
+	/*
+	 * 정수 97 -> 문자 'a'
+	 * int n = 97;
+	 * char c = (char)n;
+	 * 무작위 수 10개를 구하여 모두 결합한 문자열을 리턴하는 메서드를 작성하고
+	 * 호출해서 그 결과를 출력하고, 모음을 제외한 문자만 추출하여 문자열을 구성하여
+	 * 리턴하는 메서드
+	 */
+	private static String password() {
+		char[] tmp = new char[10];
+		String password = new String();
+		for(int i = 0;i<tmp.length;i++) {
+			tmp[i] = (char)(rd.nextInt(26)+97);
+		}
+		String result = new String(tmp, 0, 10);
+		
+		System.out.println("Password: "+result);
+		
+		for(int i = 0;i<result.length();i++) {
+			if(result.charAt(i) != 'a' && result.charAt(i) != 'e' &&
+					result.charAt(i) != 'i' && result.charAt(i) != 'o'
+					&& result.charAt(i) != 'u') {
+				password += result.charAt(i);
+			}
+		}
+		
+		System.out.println("Password changed: "+ password);
+		
+		return password;
+	}
+	
+	/*
+	 * 임의의 두 정수를 전달받았을 때, 작은 수부터 큰 수에 이르기까지의 정수합을 구하는 기능
+	 */
+	private static int sum2(int a, int b) {
+		int tmp = 0;
+		int sum = 0;
+		if(a > b) {
+			tmp= a;
+			a = b;
+			b = tmp;
+		}
+		
+		for(;a<=b ;a++) {
+			sum += a;
+		}
+		return sum;
+	}
+	
+	/*
+	 * 회원번호, 회원명을 전달하여 Member 객체를 생성하여 리턴하는 기능
+	 */
+	private static Member createM(int num, String name) {
+		Member mem = new Member();
+		mem.setNumber(num);
+		mem.setName(name);
+		return mem;
+	}
+	
+	/*
+	 * 리스트 안에 3명의 회원 정보를 저장하여 어떤 메소드에 전달하고
+	 * 전달받은 메소드에서는 그 회원들의 이름만 추출하여 리턴한다.
+	 */
+	private static String[] getNames(ArrayList<Member> memlist) {
+		String[] names = new String[memlist.size()];
+		for(int i =0; i<memlist.size(); i++) {
+			names[i] = memlist.get(i).getName();
+		}
+		return names;
+		
 	}
 }
--- src/com/tjoeun/io/DataIO.java
+++ src/com/tjoeun/io/DataIO.java
@@ -3,32 +3,78 @@ package com.tjoeun.io;
 import java.io.*;
 import java.util.*;
 
+import com.tjoeun.exception.SearchException;
 import com.tjoeun.oop.Member;
 
 public class DataIO {
 	static String fpath = "C:/labs/dataio.data";
+	static String line = "================================================";
+	static String menu = "[추가 - a\t" + "검색 - f\t" + "삭제 - d\t" + "수정 - u\t"	+ "목록 - s\t"	+ "종료 - x\t]";
+	static Scanner sc = new Scanner(System.in);
+	static LinkedList<Member> memlist = new LinkedList<>();
+	
 	public static void main(String[] args) {
 		// DataInputStream, DataOutputStream
-		Member m = loadMem();
-		m.printList();
+		loadMem();
 		
-		Member m2 = new Member(22, "Name", "010-2345-3225", "email");
-		saveMem(m2);
+		while(true) {
+			String key = null;
+			try {
+				key = menu();
+				switch(key) {
+				case "a":
+					add();
+					break;
+				case "f":
+					find().printList();
+					System.out.println();
+					break;
+				case "d":
+					delete();
+					break;
+				case "u":
+					update();
+					break;
+				case "s":
+					list();
+					break;
+				case "x":
+					break;
+				default: 
+					throw new Exception("메뉴 입력 오류");
+				}	  
+			} catch (SearchException e2) {
+				System.err.println(e2.getMessage());
+				continue;
+			} catch(IndexOutOfBoundsException ioe) {
+				System.err.println(ioe.getMessage());
+				continue;
+			} catch(Exception e){
+				System.out.println(e.getMessage());
+			}
+			
+			if(key.equals("x")) {
+				boolean check = false;
+				for(Member m:memlist) {
+					saveMem(m, check);
+					check = true;
+				}
+				System.out.println("프로그램을 종료합니다...");
+				break;
+			}
+		}
 		
-		System.out.println("프로그램 종료..");
 	}
 	
-	private static void saveMem(Member m) {
+	private static void saveMem(Member m, boolean check) {
 		try {
-			
-			DataOutputStream dout = new DataOutputStream(new FileOutputStream(fpath));
+			DataOutputStream dout = new DataOutputStream(new FileOutputStream(fpath, check));
 			dout.writeInt(m.getNumber());
 			dout.writeUTF(m.getName());
 			dout.writeUTF(m.getPhone());
 			dout.writeUTF(m.getEmail());
 			dout.close();
 			
-			System.out.println("[저장 완료]");
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -36,26 +82,197 @@ public class DataIO {
 		}
 	}
 	
-	private static Member loadMem() {
-		Member m = new Member();
+	private static void loadMem() {
+		DataInputStream din = null;
+		
 		try {
-			DataInputStream din = new DataInputStream(new FileInputStream(fpath));
-			int num = din.readInt();
-			String name = din.readUTF();
-			String phone = din.readUTF();
-			String email = din.readUTF();
-			
-			m.setNumber(num);
-			m.setName(name);
-			m.setPhone(phone);
-			m.setEmail(email);
+			din = new DataInputStream(new FileInputStream(fpath));
+			while(true) {
+				Member m = new Member();
+
+				int num = din.readInt();
+				String name = din.readUTF();
+				String phone = din.readUTF();
+				String email = din.readUTF();
+				
+				m.setNumber(num);
+				m.setName(name);
+				m.setPhone(phone);
+				m.setEmail(email);
+				memlist.add(m);
+			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
+		} catch (EOFException e) {
 		} catch (IOException e) {
 			e.printStackTrace();
+		} finally {
+			try {
+				din.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 		System.out.println("[로드 완료]");
-		return m;
+	}
+	private static String menu() throws Exception{
+		System.out.println(line);
+		System.out.println("번호\t이름\t전화번호\t\t이메일");
+		System.out.println(line);
+		
+		for(Member m:memlist) {
+			m.printList();
+		}
+		
+		System.out.println();
+		System.out.println(menu);
+		System.out.print("원하는 기능키를 입력하시오: ");
+		String key = sc.nextLine();
+		if(key.length()>=2) {
+			throw new Exception("메뉴는 1문자로 구성되어야 함");
+		}
+		return key;
+	}
+	private static void add() {
+		System.out.println("<추가 기능을 선택했습니다>");
+		
+		int num = 0;
+		String name = null;
+		String phone = null;
+		String email = null;
+		
+		while(true) {			
+			try {
+				System.out.println("번호: ");
+				num = sc.nextInt();
+				
+				System.out.println("이름: ");
+				name = sc.next();
+				
+				System.out.println("전화번호: ");
+				phone = sc.next();
+				
+				System.out.println("이메일: ");				
+				email = sc.next();
+				sc.nextLine();
+				break;
+			} catch(InputMismatchException ie) {
+				System.err.println("번호항목은 숫자만 입력해주세요.");
+				sc.nextLine();
+			} catch(Exception e) {
+				System.err.println("전화번호 형식이 틀렸습니다.");
+				sc.nextLine();
+			}
+		}
+		
+		Member mem1 = new Member();
+		mem1.setNumber(num);
+		mem1.setName(name);
+		mem1.setPhone(phone);
+		mem1.setEmail(email);
+		
+		memlist.add(mem1);
+		
+		System.out.println("[추가 완료]");
 	}
 	
+	private static void list() {
+		System.out.println("<목록 기능을 선택했습니다>");
+		System.out.println(line);
+		System.out.print("번호\t이름\t전화번호\t\t이메일\n");
+		for(Member m:memlist) {
+			m.printList();
+		}
+		System.out.println();
+		System.out.println("[결과 출력 완료]");
+		System.out.println(line);
+	}
+	
+	private static Member find() throws SearchException {
+		System.out.println("<찾기 기능을 선택했습니다>");
+		
+		System.out.println("번호로 찾으려면 m, 이름으로 찾을 때는 n번을 눌러주세요.");
+		String key_d = sc.nextLine();
+		Member result = null;
+		
+		switch(key_d) {
+		case "m":
+			System.out.printf("찾을 번호를 입력하세요: ");
+			int search_num = sc.nextInt();
+			sc.nextLine();
+			for(int i =0; i<memlist.size();i++) {
+				result = memlist.get(i);
+				if(result.getNumber() == search_num)
+					return result;
+			}
+			String msg = String.format("%s 검색결과가 없습니다.", search_num);
+			throw new SearchException(msg);
+		case "n":
+			System.out.printf("찾을 이름을 입력하세요: ");
+			String search_name = sc.nextLine();
+			
+			for(int i =0; i<memlist.size();i++) {
+				result = memlist.get(i);
+				if(result.getName().equals(search_name)) {
+					return result;
+				}
+			}
+			String msg1 = String.format("%s 검색결과가 없습니다.", search_name);
+			throw new SearchException(msg1);
+		}
+		
+		return result;
+	}
+	private static void delete() {
+		System.out.println("<추가 기능을 선택했습니다>");
+		
+		System.out.println("삭제할 번호를 입력하세요.");
+		int del_num = sc.nextInt();
+		sc.nextLine();
+		int idx = -1;
+		
+		for(int i =0; i<memlist.size();i++) {
+			Member mem = new Member();
+			mem = memlist.get(i);
+			if(mem.getNumber() == del_num)
+				idx = i;
+		}
+		memlist.remove(idx);
+		
+		System.out.println("[삭제완료]");
+		
+	}
+	private static void update() {
+		System.out.println("<수정 기능을 선택했습니다>");
+		
+		System.out.printf("변경할 사용자의 번호를 입력하세요: ");
+		int idx = sc.nextInt();
+		sc.nextLine();
+	
+		System.out.println("전화번호를 변경하려면 p, 이메일을 변경할 때는 e번을 눌러주세요.");
+		String del_k = sc.nextLine();
+		switch(del_k) {
+		case "p": 
+			System.out.printf("변경할 내용을 입력하세요: ");
+			String temp_phone = sc.nextLine();
+			for(int i =0; i<memlist.size();i++) {
+				Member mem = new Member();
+				mem = memlist.get(i);
+				if(mem.getNumber() == idx)
+					mem.setPhone(temp_phone);
+			}
+			break;
+		case "e":
+			System.out.printf("변경할 내용을 입력하세요: ");
+			String temp_email = sc.nextLine();			
+			for(int i =0; i<memlist.size();i++) {
+				Member mem = new Member();
+				mem = memlist.get(i);
+				if(mem.getNumber() == idx)
+					mem.setEmail(temp_email);
+			}
+			break;
+		}	
+		System.out.println("[변경완료]");
+	}
 }
